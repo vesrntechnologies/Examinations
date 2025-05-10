06274d36502267815444de554e3b6fbd7ce77742
vesrntechnologies <vennjoy@gmail.com>
2025-05-10 17:02:11 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,615 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the sequence, in which computer operates?",
+   
+    answer: "Input, Process, Output",
+    options: [
+      "Input, Output, Process",
+      "Input, Process, Output",
+      "Process, Input, Output",
+      "Output, Process, Input"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How does the parallel port transfer a byte?",
+    answer: "8 bits at a time",
+    options: [
+      "Bit by bit",
+      "2 bits at a time",
+      "4 bits at a time", 
+      "8 bits at a time"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is an internal power supply units of CPU?",
+    
+    answer: "SMPS",
+    options: [
+      "CVT",
+      "UPS",
+      "SMPS",
+      "Stabilizer"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the purpose of expansion slot in mother  board?",
+    answer: "To insert the additional peripherals",
+    options: [
+      "To Insert the RAM",
+      "To insert the mouse",
+      "To insert the keyboard ",
+      "To insert the additional peripherals"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which factor influences the severity of electrical shock?",
+    answer: "Duration of current passing",
+    options: [
+      "Very low DC voltage",
+      "Duration of current passing",
+      "Level of current in micro ampere",
+      "Person receives the shock"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the full form of ABC in first aid?",
+    answer: "Airway, Breathing, Circulation",
+    options: [
+      "Anyway, Breathing, Circulation ",
+      "Airway, Breathing, Circulation",
+      "Anyway, Blood, Circulation",
+      "Airway, Blood, Chest "
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the full form of ALU? ",
+    answer: "Arithmetic Logic Unit",
+    options: [
+      "Allowed Logic Unit ",
+      "AScii Logic Unit",
+      "Arithmetic Logic Unit",
+      "Arithmetic Least Unit"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is current? ",
+
+    answer: "Flow of Electrons",
+    options: [
+      "Flow of Neutrons",
+      "Flow of Electrons",
+      "Flow of Protons",
+      "Flow of Resistance"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which one is arm lift back pressure method?  ",
+
+    answer: "Nelsons method",
+    options: [
+      "Mouth to mouth method",
+      "Mouth to nose method",
+      "Nelsons method",
+      "Schafers method"
+    ]
+  },
+ {
+    numb: 10,
+    question: "Desktop and personal computer is also called as__________ ",
+
+    answer: "Micro Computer",
+    options: [
+      "Super Computer",
+      "Quantum Computer",
+      "Mainframe Computer",
+      "Micro Computer"
+    ]
+  },
+     {
+    numb: 11,
+    question: "What is the full form of SSD?",
+
+    answer: "Solid State Drive",
+    options: [
+      "Signal State Drive",
+      "Solid State Drive",
+      "Single Signal Drive",
+      "Solid Signal Drive"
+    ]
+  },
+     {
+    numb: 12,
+    question: "Which one is an example for OS?",
+
+    answer: "Windows",
+    options: [
+      "Antivirus",
+      "Windows",
+      "MS Office",
+      "Macromedia"
+    ]
+  },
+     {
+    numb: 13,
+    question: "Which control panel applet gives the information of computer?",
+
+    answer: "System and Security",
+    options: [
+      "System and Security",
+      "Hardware and sound",
+      "Programs",
+      "Appearance and personalization"
+    ]
+  },
+     {
+    numb: 14,
+    question: "Which shortcut key is used to copy and paste a file folder? ",
+
+    answer: "Ctrl+ C and Ctrl+ V",
+    options: [
+      "Ctrl+X and Ctrl+ V",
+      "Ctrl + A and Ctrl + V",
+      "Ctrl + Z and Ctrl + V",
+      "Ctrl+ C and Ctrl+ V"
+    ]
+  },
+     {
+    numb: 15,
+    question: "Which is the order of files and directories in Window Explore? ",
+
+    answer: "Hierarchically",
+    options: [
+      "Serially",
+      "Sequentially",
+      "Hierarchically",
+      "Alphabetically"
+    ]
+  },
+
+     {
+    numb: 16,
+    question: "Where does the pinned applications stayed in Windows OS? ",
+
+    answer: "Taskbar",
+    options: [
+      "Desktop",
+      "Notification Area",
+      "Start Button",
+      "Taskbar"
+    ]
+  },
+     {
+    numb: 17,
+    question: "What is the extension of notepad file? ",
+
+    answer: ".txt",
+    options: [
+      ".fil",
+      ".dll",
+      ".doc",
+      ".txt"
+    ]
+  },
+     {
+    numb: 18,
+    question: "Name the tool in control panel which is used to adjust your computer setting with voice command? ",
+
+    answer: "Ease of access",
+    options: [
+      "Ease of access",
+      "System and Security",
+      "Appearance and personalization",
+      "Hardware and sound"
+    ]
+  },
+     {
+    numb: 19,
+    question: "Name the shortcut key used to open start menu? ",
+
+    answer: "Windows Logo",
+    options: [
+      "Windows Logo",
+      "Ctrl",
+      "Shift",
+      "Alt"
+    ]
+  },
+     {
+    numb: 20,
+    question: "Which type of device is a printer? ",
+
+    answer: "Output Device",
+    options: [
+      "Input device",
+      "Output Device",
+      "Processing Device",
+      "Calcuating Device"
+    ]
+  },
+     {
+    numb: 21,
+    question: "What is the function of operating system? ",
+
+    answer: "Process and memory management",
+    options: [
+      "Word processing",
+      "Calculation",
+      "Process and memory management",
+      "Drawing Pictures"
+    ]
+  },
+     {
+    numb: 22,
+    question: "Name the ICON in which deleted files and folder are found",
+
+    answer: "Recycle Bin",
+    options: [
+      "My Computer",
+      "Recycle Bin",
+      "Microsoft Edge",
+      "Google Chrome"
+    ]
+  },
+     {
+    numb: 23,
+    question: "Name the tool in control panel which we can change the system date?",
+
+    answer: "Date and Time",
+    options: [
+      "User Account",
+      "System",
+      "Date and Time",
+      "Fonts"
+    ]
+
+         
+  },
+     {
+    numb: 24,
+    question: "Name the shortcut key used for refresh windows desktop ",
+
+    answer: "F5",
+    options: [
+      "F1",
+      "F3",
+      "F5",
+      "F7"
+    ]
+  },
+     {
+    numb: 25,
+    question: "Which type of device is a scanner? ",
+
+    answer: "Input device",
+    options: [
+      "Processing Device",
+      "Internal Device",
+      "Output Device",
+      "Input device"
+    ]
+  },
+     {
+    numb: 26,
+    question: "Which type of copy is produced by printer? ",
+
+    answer: "Hard Copy",
+    options: [
+      "Hard Copy",
+      "Soft Copy",
+      "Scan Copy",
+      "Xerox Copy"
+    ]
+  },
+     {
+    numb: 27,
+    question: "What is the standard file extension of scan image?",
+
+    answer: ".jpg",
+    options: [
+      ".exe",
+      ".doc",
+      ".bak",
+      ".jpg"
+    ]
+  },
+     {
+    numb: 28,
+    question: "What is the capacity of typical DVD?",
+
+    answer: "4.7GB",
+    options: [
+      "40MB",
+      "50MB",
+      "60MB",
+      "4.7GB"
+    ]
+  },
+     {
+    numb: 29,
+    question: "What is BIOS?",
+
+    answer: "Firmware",
+    options: [
+      "Firmware",
+      "Hardware",
+      "Middleware",
+      "Software"
+    ]
+  },
+     {
+    numb: 30,
+    question: "What is the purpose of Nero Start Smart? ",
+
+    answer: "Burn CD/DVD",
+    options: [
+      "Format HDD",
+      "Burn CD/DVD",
+      "Format Pendrive",
+      "Format CD/DVD"
+    ]
+  },
+     {
+    numb: 31,
+    question: "Which System is used by Bluetooth for broad casting?",
+
+    answer: "Radio waves",
+    options: [
+      "Satellite",
+      "Microwaves",
+      "Telephone lines",
+      "Radio waves"
+    ]
+  },
+     {
+    numb: 32,
+    question: "What is the process of deleting all the data on the hard drive? ",
+
+    answer: "Formatting",
+    options: [
+      "Delete",
+      "Erase",
+      "Formatting",
+      "Uninstall"
+    ]
+  },
+ {
+    numb: 33,
+    question: "Which port is used to connect monitor?",
+
+    answer: "VGA",
+    options: [
+      "PS2",
+      "USB",
+      "LAN",
+      "VGA"
+    ]
+  },    
+     {
+    numb: 34,
+    question: "Computer program are known as",
+
+    answer: "software",
+    options: [
+      "Operating System",
+      "Applications",
+      "software",
+      "icon"
+    ]
+  },
+     {
+    numb: 35,
+    question: "Statical analysis packages have much in common with__ ",
+
+    answer: "Spreadsheet package",
+    options: [
+      "Spreadsheet package",
+      "Presentation package",
+      "Design packages",
+      "Paint package"
+    ]
+  },
+     {
+    numb: 36,
+    question: "Which software interface the major hardware components of the computer with OS",
+
+    answer: "BIOS",
+    options: [
+      "BIOS",
+      "DOS",
+      "POST",
+      "CMOS"
+    ]
+  },
+     {
+    numb: 37,
+    question: "Which software acts as a bridge between system software and hardware?",
+
+    answer: "Application",
+    options: [
+      "Management",
+      "Application",
+      "Utilty",
+      "Processing"
+    ]
+  },
+     {
+    numb: 38,
+    question: "Which virus infects the important files of application software?",
+
+    answer: "Macro Virus",
+    options: [
+      "Macro Virus",
+      "Trojan Virus",
+      "Worm Virus",
+      "Anti Virus"
+    ]
+  },
+     {
+    numb: 39,
+    question: "Which DOS command is used to delete a directory?",
+
+    answer: "RD",
+    options: [
+      "RD",
+      "Remove",
+      "DEL",
+      "DELETE"
+    ]
+  },
+     {
+    numb: 40,
+    question: "Which character is used to represent parent directory in Linux?",
+
+    answer: "..",
+    options: [
+      ".",
+      "..",
+      "~",
+      "*"
+    ]
+  },
+     {
+    numb: 41,
+    question: "Which character is used to redirect output in to an existing file in Linux? ",
+
+    answer: ">",
+    options: [
+      ">",
+      ">>",
+      "<",
+      "<<"
+    ]
+  },
+     {
+    numb: 42,
+    question: "How the directories under the root directory are called",
+
+    answer: "sub directories",
+    options: [
+      "dir",
+      "path",
+      "sub directories",
+      "tree"
+    ]
+  },
+     {
+    numb: 43,
+    question: "Which filename will matches the command. DIR????T.TxT in DOS?",
+
+    answer: "TEMPT.TxT",
+    options: [
+      "TIME.TXT",
+      "TEXT.TXT",
+      "TEMP.TXT",
+      "TEMPT.TxT"
+    ]
+  },
+     {
+    numb: 44,
+    question: "Which one of the following is the wildcard character?",
+
+    answer: "* and ?",
+    options: [
+      "* and /",
+      "* and ?",
+      "? and @",
+      "? and /"
+    ]
+  },
+     {
+    numb: 45,
+    question: "Which character is used to represent a range of values in Linux?",
+
+    answer: "[]",
+    options: [
+      "()",
+      "[]",
+      "{}",
+      "<>"
+    ]
+  },
+     {
+    numb: 46,
+    question: "Who developed by the Python language? ",
+
+    answer: "Guido van Rossum",
+    options: [
+      "Guido van Rossum",
+      "Zim Den",
+      "Wick Van Rossum",
+      "Niene Stom"
+    ]
+  },
+     {
+    numb: 47,
+    question: "Which multimedia application was developed using Python?",
+
+    answer: "Tim Player",
+    options: [
+      "Matplot lib",
+      "Pandas",
+      "Tryton",
+      "Tim Player"
+    ]
+  },
+     {
+    numb: 48,
+    question: "Whether Python Code compiled or interpreted?",
+
+    answer: "Python code is only interpreted",
+    options: [
+      "Python code is only interpreted",
+      "Python code is neither compiled nor interpreted",
+      "Python code is only compiled",
+      "Python code is both compiled and interpreted"
+    ]
+  },
+     {
+    numb: 49,
+    question: "Which of the following character is used to give single-line comment in Python? ",
+
+    answer: "#",
+    options: [
+      "//",
+      "#",
+      "!",
+      "/*"
+    ]
+  },
+     {
+    numb: 50,
+    question: "What is the data type of print(type(0xFF))? ",
+
+    answer: "int",
+    options: [
+      "number",
+      "hexint",
+      "hex",
+      "int"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
